175ea388356cd8553ca6ff1d4bc02a84d4f0fd9e
Dan LaManna <dan.lamanna@kitware.com>
2019-07-30 12:02:28 -0400

    Fix diagnoses

--- algorithm.py
+++ algorithm.py
@@ -4,18 +4,19 @@ import sys
 
 random.seed(90210)
 
-diagnoses = ['MEL', 'NV', 'BCC', 'AKIEC', 'BKL', 'DF', 'VASC']
+diagnoses = ['MEL', 'NV', 'BCC', 'AK', 'BKL', 'DF', 'VASC', 'SCC', 'UNK']
 
 
 def my_algorithm(image):
     return [random.random() for _ in range(len(diagnoses))]
+
 
 if not os.path.exists('/images'):
     print('The directory /images does not exist, did you forget to mount it?', file=sys.stderr)
     sys.exit(1)
 
 # print headers
-print(','.join(['image'] + diseases))
+print(','.join(['image'] + diagnoses))
 
 # print scores for each image
 for input_file in os.listdir('/images'):
